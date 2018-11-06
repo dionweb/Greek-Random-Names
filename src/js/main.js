22acab5da20aa3c0987050af34bad01884432e10
Dionysios Tsentidis <dionysiostsentidis@gmail.com>
2018-11-06 12:59:55 +0200

    Changed some styles

--- src/js/main.js
+++ src/js/main.js
@@ -19,66 +19,66 @@ function generateName() {
 
     if (firstValue == 'male' && secondValue == 'popular'){
         var randomnumber = Math.floor(Math.random() * (malePopular.length));
-        document.getElementById('result').innerHTML = malePopular[randomnumber] + '<br> <br> is a popular greek male name!';
+        document.getElementById('result').innerHTML = malePopular[randomnumber] + '<br> is a popular greek male name!';
     }
     else if (firstValue == 'male' && secondValue == 'rare'){
         var randomnumber = Math.floor(Math.random() * (maleRare.length));
-        document.getElementById('result').innerHTML = maleRare[randomnumber] + '<br> <br> is a rare greek male name!';    
+        document.getElementById('result').innerHTML = maleRare[randomnumber] + '<br> is a rare greek male name!';    
     }
     else if (firstValue == 'male' && secondValue == 'ancient'){
         var randomnumber = Math.floor(Math.random() * (maleAncient.length));
-        document.getElementById('result').innerHTML = maleAncient[randomnumber] + '<br> <br> is an ancient greek male name!';   
+        document.getElementById('result').innerHTML = maleAncient[randomnumber] + '<br> is an ancient greek male name!';   
     }
     else if ((firstValue == 'male' && secondValue == 'idontcare') || (firstValue == 'male' && secondValue == 'null')){
         var dontcare = malePopular.concat(maleRare).concat(maleAncient);
         var randomnumber = Math.floor(Math.random() * (dontcare.length));
-        document.getElementById('result').innerHTML = dontcare[randomnumber] + '<br> <br> is a greek male name!';
+        document.getElementById('result').innerHTML = dontcare[randomnumber] + '<br> is a greek male name!';
     }
 
     if (firstValue == 'female' && secondValue == 'popular'){
         var randomnumber = Math.floor(Math.random() * (malePopular.length));
-        document.getElementById('result').innerHTML = femalePopular[randomnumber] + '<br> <br> is a popular greek female name!';
+        document.getElementById('result').innerHTML = femalePopular[randomnumber] + '<br> is a popular greek female name!';
     }
     else if (firstValue == 'female' && secondValue == 'rare'){
         var randomnumber = Math.floor(Math.random() * (maleRare.length));
-        document.getElementById('result').innerHTML = femaleRare[randomnumber] + '<br> <br> is a rare greek female name!';    
+        document.getElementById('result').innerHTML = femaleRare[randomnumber] + '<br> is a rare greek female name!';    
     }
     else if (firstValue == 'female' && secondValue == 'ancient'){
         var randomnumber = Math.floor(Math.random() * (maleAncient.length));
-        document.getElementById('result').innerHTML = femaleAncient[randomnumber] + '<br> <br> is an ancient greek female name!';   
+        document.getElementById('result').innerHTML = femaleAncient[randomnumber] + '<br> is an ancient greek female name!';   
     }
     else if ((firstValue == 'female' && secondValue == 'idontcare') || (firstValue == 'female' && secondValue == 'null')){
         var dontcare = femalePopular.concat(femaleRare).concat(femaleAncient);
         var randomnumber = Math.floor(Math.random() * (dontcare.length));
-        document.getElementById('result').innerHTML = dontcare[randomnumber] + '<br> <br> is a greek female name!';
+        document.getElementById('result').innerHTML = dontcare[randomnumber] + '<br> is a greek female name!';
     }
 
 
     if ((firstValue == 'idontcare' && secondValue == 'popular') || (firstValue == 'null' && secondValue == 'popular')){
         var dontcare = malePopular.concat(femalePopular);
         var randomnumber = Math.floor(Math.random() * (dontcare.length));
-        document.getElementById('result').innerHTML = dontcare[randomnumber] + '<br> <br> is a popular greek name!';
+        document.getElementById('result').innerHTML = dontcare[randomnumber] + '<br> is a popular greek name!';
     }
     else if ((firstValue == 'idontcare' && secondValue == 'rare') || (firstValue == 'null' && secondValue == 'rare')){
         var dontcare = maleRare.concat(femaleRare);
         var randomnumber = Math.floor(Math.random() * (dontcare.length));
-        document.getElementById('result').innerHTML = dontcare[randomnumber] + '<br> <br> is a rare greek name!';
+        document.getElementById('result').innerHTML = dontcare[randomnumber] + '<br> is a rare greek name!';
     }
     else if ((firstValue == 'idontcare' && secondValue == 'ancient') || (firstValue == 'null' && secondValue == 'ancient')){
         var dontcare = maleAncient.concat(femaleAncient);
         var randomnumber = Math.floor(Math.random() * (dontcare.length));
-        document.getElementById('result').innerHTML = dontcare[randomnumber] + '<br> <br> is an ancient greek name!'; 
+        document.getElementById('result').innerHTML = dontcare[randomnumber] + '<br> is an ancient greek name!'; 
     }
     else if ((firstValue == 'idontcare' && secondValue == 'idontcare') || (firstValue == 'idontcare' && secondValue == 'null') || (firstValue == 'null' && secondValue == 'idontcare')){
         var dontcare = malePopular.concat(maleRare).concat(maleAncient).concat(femalePopular).concat(femaleRare).concat(femaleAncient);
         var randomnumber = Math.floor(Math.random() * (dontcare.length));
-        document.getElementById('result').innerHTML = dontcare[randomnumber] + '<br> <br> is a greek name!';
+        document.getElementById('result').innerHTML = dontcare[randomnumber] + '<br> is a greek name!';
     }
 
     if (firstValue == 'null' && secondValue == 'null'){
         var dontcare = malePopular.concat(maleRare).concat(maleAncient).concat(femalePopular).concat(femaleRare).concat(femaleAncient);
         var randomnumber = Math.floor(Math.random() * (dontcare.length));
-        document.getElementById('result').innerHTML = 'Υou didn\'t choose a category. I print whatever i want.<br> <br>' + dontcare[randomnumber];
+        document.getElementById('result').innerHTML = 'Υou didn\'t choose a category. I print whatever i want.<br>' + dontcare[randomnumber];
     }
 
 }   
